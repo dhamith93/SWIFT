4c231087dbd2f4524c6f9cafef662b42f5ca2a5a
Dhamith Hewamullage <hewamullage123@gmail.com>
2019-01-04 11:26:21 +0530

    change config for linux dev env

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // DHAMITH
-$config['base_url'] = 'http://localhost:8888/SWIFT/';
+$config['base_url'] = 'http://localhost/SWIFT/';
 
 // PRABUDDHA
 // $config['base_url'] = 'http://localhost:8888/SWIFT/';
@@ -384,7 +384,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
