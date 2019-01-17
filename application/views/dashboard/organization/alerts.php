2207c893bbf0c202be31519cb3b122a4dca73f33
Dhamith Hewamullage <hewamullage123@gmail.com>
2019-01-17 11:26:51 +0530

    Organization disply tables only if not empty

--- application/views/dashboard/organization/alerts.php
+++ application/views/dashboard/organization/alerts.php
@@ -1,21 +1,25 @@
 <div class="section">
-    <table class="table is-bordered is-striped is-narrow is-hoverable" style="margin: auto;">
-        <thead>
-            <tr>
-                <th>Incident</th>
-                <th>Alert</th>
-                <th>Date Time</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php 
-                foreach ($alerts as $row) {
-                    echo '<tr>';
-                    echo '<td><a href="../../incident/' . $row->inc_id . '" target="_blank">' . ucfirst($row->name) . '</a></td>';
-                    echo '<td style="max-width: 600px;">' . ucfirst($row->content) . '</td>';
-                    echo '<td>' . ucfirst($row->published_date) . '</td>';
-                }
-            ?>
-        </tbody>
-    </table>
+    <?php if (!empty($alerts)) : ?>
+        <table class="table is-bordered is-striped is-narrow is-hoverable" style="margin: auto;">
+            <thead>
+                <tr>
+                    <th>Incident</th>
+                    <th>Alert</th>
+                    <th>Date Time</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                    foreach ($alerts as $row) {
+                        echo '<tr>';
+                        echo '<td><a href="../../incident/' . $row->inc_id . '" target="_blank">' . ucfirst($row->name) . '</a></td>';
+                        echo '<td style="max-width: 600px;">' . ucfirst($row->content) . '</td>';
+                        echo '<td>' . ucfirst($row->published_date) . '</td>';
+                    }
+                ?>
+            </tbody>
+        </table>
+    <?php else : ?>
+        <p style="text-align:center;">No alerts yet...</p>
+    <?php endif; ?>
 </div>
