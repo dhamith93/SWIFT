e31b841aeb2f5b3ade5c717537d314e279be4c29
Dhamith Hewamullage <hewamullage123@gmail.com>
2019-02-03 04:33:27 +0000

    remote config

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // DHAMITH
-$config['base_url'] = 'http://localhost/SWIFT/';
+$config['base_url'] = 'https://dhamith.me/SWIFT/';
 
 // PRABUDDHA
 // $config['base_url'] = 'http://localhost:8888/SWIFT/';
