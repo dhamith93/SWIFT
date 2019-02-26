44229b780d2910573bfcb9b8c7059262b9bc7e3c
Prabuddha <prabuddha.proart@gmail.com>
2019-02-26 23:00:59 +0530

    reports link in home page

--- application/views/public/home.php
+++ application/views/public/home.php
@@ -2,10 +2,7 @@
             <div class="header__text-box">
                 <h1 class="heading-primary">
                     <span class="heading-primary--main">Disaster Management Center</span>
-                    <span class="heading-primary--sub"><a href="#" class="heading-primary--sub--link">somelink <span
-                                class="heading-primary--sub--link--arrow">&rarr;</span></a> </span>
-                    <span class="heading-primary--sub"><a href="#" class="heading-primary--sub--link">somelink &rarr;</a></span>
-                    <span class="heading-primary--sub"><a href="#" class="heading-primary--sub--link">somelink &rarr;</a></span>
+    
                 </h1>
             </div>
             
@@ -16,9 +13,7 @@
         <div class="section-one">
             <div class="section-one__title">Alerts</div>
             <div class="section-one__alerts">
-                <!-- <div class="section-one__alert-box" id="section-one__alert-box">
-                    <p id="section-one__alert-box-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente itaque quas placeat ipsa voluptates aspernatur provident accusamus est velit laborum pariatur impedit porro, ratione officiis! - Date <span class="section-one__alert-box-date">Date</span></p>
-                </div> -->
+                
                
                 
             </div>
@@ -27,7 +22,7 @@
             <div class="section-two__title">Reports</div>
             <div class="section-two__report-sec">
                 <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, ratione consequatur reiciendis quisquam suscipit eos earum minima facilis nobis id?</h3>
-                <div class="section-two__report"><div class="section-two__report-text">Link</div></div>
+                <a href="<?php echo base_url(); ?>reports" style="text-decoration:none;"><div class="section-two__report"><div class="section-two__report-text">Link</div></div></a>
                 
 
             </div>
